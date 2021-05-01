b8f1912af500b8e7c0ec6849786ac14384e24608
Tanabe <tanabe2852@yahoo.co.jp>
2021-05-01 19:24:19 +0900

    課題７－１

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-let x = 0
-let y = 0
 basic.forever(function () {
-    for (let k = 0; k <= 9; k++) {
-        x = randint(0, 4)
-        y = randint(0, 4)
-        led.plot(x, y)
-        basic.pause(1000)
+    for (let y = 0; y <= 9; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(1000)
+        }
     }
     basic.clearScreen()
 })
