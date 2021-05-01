fcbb7b27b6ac3ec148d36584921fb96dc0ef4b7e
Tanabe <tanabe2852@yahoo.co.jp>
2021-05-01 19:04:37 +0900

    課題6-2-3

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+let x = 0
 basic.forever(function () {
-    basic.showString("Hello,Wrold!")
+    x = 0
+    for (let y = 0; y <= 4; y++) {
+        led.plot(x, y)
+        basic.pause(1000)
+        x += 1
+    }
+    x = 4
+    for (let y = 0; y <= 4; y++) {
+        led.plot(x, y)
+        basic.pause(1000)
+        x = x - 1
+    }
+    basic.clearScreen()
 })
